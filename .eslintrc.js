eee524208e5071b52a04bcfe2cb3b2584fef46c6
Juan Hapes <juan.hapes@janis.im>
2025-08-11 18:28:05 +0200

    Added consumers (changed from lambda) [WIP]

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
 
 	parserOptions: {
 		sourceType: 'script',
-		ecmaVersion: 2020
+		ecmaVersion: 2022
 	},
 
 	rules: {
@@ -34,6 +34,7 @@ module.exports = {
 		'consistent-return': 0,
 		'prefer-template': 0,
 		'import/no-unresolved': 0,
+		'import/extensions': 0,
 		'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/tests/**/*.js'] }],
 
 		'no-bitwise': 0,
